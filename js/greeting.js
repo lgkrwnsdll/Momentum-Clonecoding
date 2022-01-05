97cbdb798903297a6ef1edfe10edd81d00a1b8d4
lgkrwnsdll <80141718+lgkrwnsdll@users.noreply.github.com>
2022-01-06 01:22:08 +0900

    showing only login part at first
    more cloning

--- js/greeting.js
+++ js/greeting.js
@@ -1,28 +1,37 @@
-const loginForm = document.querySelector('#login-form')
-const loginInput = document.querySelector('#login-form input')
-const greeting = document.querySelector('#greeting')
+import { clock } from "./clock.js";
+import { quote, author } from "./quotes.js";
+import { toDoForm, toDoInput, toDoList } from "./todo.js";
 
-const HIDDEN_CLASSNAME = 'hidden'
-const USERNAME_KEY = 'username'
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault()
-    loginForm.classList.add(HIDDEN_CLASSNAME)
-    const username = loginInput.value
-    localStorage.setItem(USERNAME_KEY, username)
-    paintGreeting(username)
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintOthers(username);
 }
 
-function paintGreeting (username) {
-    greeting.innerText = `Hello ${username}`
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+function paintOthers(username) {
+  greeting.innerText = `Hello, ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  clock.classList.remove(HIDDEN_CLASSNAME);
+  quote.classList.remove(HIDDEN_CLASSNAME);
+  author.classList.remove(HIDDEN_CLASSNAME);
+  toDoForm.classList.remove(HIDDEN_CLASSNAME);
+  toDoList.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const savedUsername = localStorage.getItem(USERNAME_KEY)
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-    loginForm.classList.remove(HIDDEN_CLASSNAME)
-    loginForm.addEventListener('submit', onLoginSubmit)
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreeting(savedUsername)
+  paintOthers(savedUsername);
 }
--- js/todo.js
+++ js/todo.js
@@ -1,54 +1,56 @@
-const toDoForm = document.querySelector('#todo-form')
-const toDoInput = toDoForm.querySelector('input')
-const toDoList = document.querySelector('#todo-list')
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector("#todo-list");
 
-const TODOS_KEY = 'todos'
+const TODOS_KEY = "todos";
 
-let toDos = []
+let toDos = [];
 
 function saveToDos() {
-    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
-    const li = event.target.parentElement
-    li.remove()
-    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id))
-    saveToDos()
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintTodo(newTodo) {
-    const li = document.createElement('li')
-    li.id = newTodo.id
-    const span = document.createElement('span')
-    span.innerText = newTodo.text
-    const button = document.createElement('button')
-    button.innerText = '❌'
-    button.addEventListener('click', deleteToDo)
-    li.appendChild(span)
-    li.appendChild(button)
-    toDoList.appendChild(li)
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
 }
 
 function handleTodoSubmit(event) {
-    event.preventDefault()
-    const newTodo = toDoInput.value
-    toDoInput.value = ''
-    const newToDoObject = {
-        text: newTodo,
-        id: Date.now()
-    }
-    toDos.push(newToDoObject)
-    paintTodo(newToDoObject)
-    saveToDos()
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newToDoObject = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newToDoObject);
+  paintTodo(newToDoObject);
+  saveToDos();
 }
 
-toDoForm.addEventListener('submit', handleTodoSubmit)
+toDoForm.addEventListener("submit", handleTodoSubmit);
 
-const savedToDos = localStorage.getItem(TODOS_KEY)
+const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
-    const parsedToDos = JSON.parse(savedToDos)
-    toDos = parsedToDos
-    parsedToDos.forEach(paintTodo)
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintTodo);
 }
+
+export { toDoForm, toDoInput, toDoList };
